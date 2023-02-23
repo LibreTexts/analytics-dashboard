628997e482a41b8edc205e1d8dfbc3312fa7b2e1
amydahlberg <49294266+amydahlberg@users.noreply.github.com>
2023-02-23 13:28:27 -0800

    Update adaptLevelsCall.js

--- LibreTextAnalyticsDash/api/routes/adaptLevelsCall.js
+++ LibreTextAnalyticsDash/api/routes/adaptLevelsCall.js
@@ -21,6 +21,22 @@ const adaptLevelsCall = async (
   axios(config)
     .then(function (response) {
       let newData = response.data;
+      if (newData['documents'].filter(obj => obj._id === null).length > 0) {
+        newData['documents'].forEach((doc, index) => {
+          if (doc._id === null) {
+            if (doc.level_name && doc.level_name.length > 0) {
+              doc.level_name.forEach((level) => {
+                var entry = {
+                  _id: level,
+                  level_name: [level]
+                }
+                newData['documents'].push(entry)
+              })
+              newData['documents'].splice(index, 1)
+            }
+          }
+        })
+      }
       newData["adaptLevels"] = newData["documents"];
       delete newData["documents"];
       newData = JSON.stringify(newData);
